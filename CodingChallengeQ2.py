4cc74a97be249488e125d1ffc2dbab6d14b3d113
BioPneub <pne.thompson@gmail.com>
2022-04-24 22:13:03 -0700

    created function for codingchallengeq2

--- CodingChallengeQ2.py
+++ CodingChallengeQ2.py
@@ -31,3 +31,40 @@ Casesgrp = Cases.groupby(['Provider Name'])
 Case_Cumsum_Grp = Casesgrp['Provider Name','cumsum'].apply(lambda x: x.cumsum())
     #Applying the group to a variable and also using the lambda function to on the cumsum column, reitertaing for each group, for later recall. These last 4 lines were the most difficult. Really required a lot of googling youtube and
     #thinking in different ways to try and approach the problem.
+
+
+
+---------------------------
+
+# import pandas as pd
+#
+# def datareadin(filepath):
+#     global basenursinghomedata, cases
+#
+#     basenursinghomedata = pd.read_pickle(filepath)
+#     basenursinghomedata['Week Ending'] = pd.to_datetime(basenursinghomedata['Week Ending'])
+#     cases = basenursinghomedata[['Week Ending', 'Provider Name', 'Residents Weekly Confirmed COVID-19']]
+#     cases.drop_duplicates(inplace=True)
+#     cases.sort_values(['Provider Name', 'Week Ending'], ascending=[True, True], inplace=True)
+#     cases.rename(columns={'Residents Weekly Confirmed COVID-19': 'C_Cases'}, inplace=True)
+
+#     return cases
+#
+# def streaks(facilitydata):
+#     global case_cumsum_crp
+#
+#     cases['C_Cases_Shifted'] = cases['C_Cases'].shift()
+#     cases['cumsum'] = cases['C_Cases'] != cases['C_Cases_Shifted']
+#     casesgrp = cases.groupby(['Provider Name'])
+#     case_cumsum_crp = casesgrp['Provider Name', 'cumsum'].apply(lambda x: x.cumsum())
+
+#     return case_cumsum_crp
+#
+#
+# testdata = (r'\\wsl$\fedora\home\phil\Coding Challenge\NursingHomeData2021pkl\EnsignNursingHomeData2021Vacc.pkl')
+
+
+
+
+
+
